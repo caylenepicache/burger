258457ba248042d1b173d1992cd93011e3cb4217
unknown <caylenepicache@gmail.com>
2018-05-03 20:08:05 -0700

    files added

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Cheeseburger", false),
+("Hamburger", false),
+("Turkey Burger", false);
+
+
+SELECT * FROM burgers_db.burgers;
