2d7ad7af2470e1e8ccece2d22952ac542d9c754c
unknown <caylenepicache@gmail.com>
2018-05-05 09:26:52 -0700

    server

--- config/connection.js
+++ config/connection.js
@@ -3,9 +3,9 @@ var mysql = require("mysql");
 
 var connection;
 
-//if (process.env.JAWSDB_URL) {
-//  connection = mysql.createConnection(process.env.JAWSDB_URL);
-//} else {
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
   connection = mysql.createConnection({
   port: 3306,
   host: "localhost",
