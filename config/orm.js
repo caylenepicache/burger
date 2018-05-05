270ff3e8d93e1ca0288de796b3c8d5ff3b386fc2
unknown <caylenepicache@gmail.com>
2018-05-04 21:12:30 -0700

    adding files etc

---
+++ config/orm.js
@@ -0,0 +1,38 @@
+var connection = require("../config/connection");
+
+var orm = {
+    selectAll: function(burgersTable) {
+        var queryString = "SELECT * FROM ??" ;
+        console.log(queryString);
+
+        connection.query(queryString, [burgersTable], function(err, result) {
+            if (err) throw err;
+            console.log(result);
+        });
+    },
+    insertOne: function(newBurger, callback) {
+        var queryString = "INSERT INTO burgers (burger_name, devoured) VALUES ?";
+        console.log(queryString);
+
+        connection.query(queryString, newBurger, function (err, res) {
+                if (err) throw err;
+                console.log(res.body.burger_name + " inserted...");
+                callback(res);
+            });
+    },
+    updateOne: function(burgerId, devoured, callback) {
+        var queryString = "UPDATE burgers SET devoured=? WHERE id=?";
+        console.log(queryString);
+
+        devoured = parseInt(devoured);
+        burgerId = parseInt(burgerId);
+        
+        connection.query(queryString, [devoured, burgerId], function (err, res) {
+            if (err) throw err;
+            console.log(burgerId + " devoured: " + devoured);
+            callback(res);
+        });
+    }
+};
+
+module.exports = orm;
--- db/schema.sql
+++ db/schema.sql
@@ -9,6 +9,6 @@ USE burgers_db;
 CREATE TABLE burgers (
   id INT NOT NULL AUTO_INCREMENT,
   burger_name VARCHAR(100) NOT NULL,
-  devoured BOOLEAN,
+  devoured BOOLEAN default false,
   PRIMARY KEY (id)
 );
